1812b3718daaad09ec12e92f1caa709200181e9c
Pablo Gonzalez <pablo@odd.co>
2021-10-22 23:24:04 -0300

    solucion lesson3 exercise3

--- lesson3_ml_pipeline_deploy/exercise03/train.py
+++ lesson3_ml_pipeline_deploy/exercise03/train.py
@@ -1,8 +1,12 @@
 import numpy as np
 import pandas as pd
+import yaml
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score
+
+with open('params.yaml') as file:
+    params = yaml.load(file, Loader=yaml.FullLoader)
 
 X = np.loadtxt("X.csv")
 y = np.loadtxt("y.csv")
@@ -11,7 +15,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.25, random_state=23
 )
 
-lr = LogisticRegression(C=1.0)
+lr = LogisticRegression(C=params['train']['lr_C'])
 lr.fit(X_train.reshape(-1, 1), y_train)
 
 preds = lr.predict(X_test.reshape(-1, 1))
